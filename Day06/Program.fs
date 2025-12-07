1b9fdf7c23f8de3edb1bbe3bfff97c5e0e51068a
Nicholas Bannayi <nbannayi@hotmail.co.uk>
2025-12-07 13:36:11 +0000

    Day 6 pt 1

---
+++ Day06/Program.fs
@@ -0,0 +1,26 @@
+﻿open System.IO
+
+// Day 6: Trash Compactor.
+let parseSums1 inputFile = 
+    File.ReadAllLines inputFile    
+    |> Array.map (fun i ->
+        i.Split(' ') |> Array.filter (fun j -> j.Trim() <> ""))
+    |> Array.transpose
+
+let getGrandTotal inputSums =
+    inputSums
+    |> Array.map (fun i ->            
+        let operands = i |> Array.take(4) |> Array.map (int64)
+        let operator = i.[4]
+        match operator with
+        | "+" -> operands |> Array.reduce (+)
+        | _   -> operands |> Array.reduce (*))
+    |> Array.sum
+
+[<EntryPoint>]
+let main argv =
+    let fileName = "Day06.txt"
+
+    let inputSums1 = parseSums1 fileName    
+    printfn $"Part 1 answer: {getGrandTotal inputSums1}"
+    0
