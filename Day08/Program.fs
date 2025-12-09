ed1a515cb967869c49e7d31eda478258802dc8cc
Nicholas Bannayi <nbannayi@hotmail.co.uk>
2025-12-09 23:36:14 +0000

    Day 8 + half of 9

---
+++ Day08/Program.fs
@@ -0,0 +1,89 @@
+﻿open System.IO
+open System
+
+// Day 8: Playground.
+let getDistance (coord1: (int * int * int)) (coord2: (int * int * int)) =
+    let (x1,y1,z1) = coord1
+    let (x2,y2,z2) = coord2 
+    Math.Sqrt((double (x2-x1)) ** 2. + (double (y2-y1)) ** 2. + (double (z2-z1)) ** 2.)
+
+let getShortestCombos (coords : (int * int * int) array) n =
+    let combos =
+        [|for i in 0..coords.Length-1 do
+            for j in i+1..coords.Length-1 ->
+                getDistance coords.[i] coords.[j], (coords.[i], coords.[j])|]
+        |> Array.sortBy (fun (d,_) -> d)
+    if n > 0 then
+        combos |> Array.take(n) 
+    else
+        combos
+    |> Array.map (fun (_,coords) -> coords)
+
+let partitionCoords (combos: ((int * int * int) * (int * int * int)) array) =
+    let partitions = ResizeArray<(int * int * int) list>()    
+    combos
+    |> Array.iter (fun (c1,c2) ->
+        let candidateIndex1 = 
+            partitions
+            |> Seq.tryFindIndex (fun p -> p |> List.contains c1)
+        let candidateIndex2 = 
+            partitions
+            |> Seq.tryFindIndex (fun p -> p |> List.contains c2)
+        match candidateIndex1, candidateIndex2 with
+        | Some i, None -> partitions.[i] <- partitions.[i] @ [c1;c2]
+        | None, Some i -> partitions.[i] <- partitions.[i] @ [c1;c2]
+        | Some i, Some j ->
+            if i <> j then
+                partitions.[i] <- partitions.[i] @ partitions.[j] @ [c1;c2]
+                partitions.RemoveAt(j)
+        | _ -> partitions.Add([c1;c2]))
+    partitions
+    |> Seq.map (fun l -> l |> List.distinct)
+    |> Array.ofSeq
+
+let getClosedCircuitSize (combos: ((int * int * int) * (int * int * int)) array) =
+    let partitions =
+        combos
+        |> partitionCoords    
+    partitions.[0] |> List.length
+    
+[<EntryPoint>]
+let main argv =    
+    let noPairs = 1000 // Change for example vs. actual data.    
+    let closedCircuitSize = 1000
+
+    let coords =
+        File.ReadAllLines "Day08.txt"
+        |> Array.map (fun l ->
+            let coords = l.Split(",")
+            (int coords.[0], int coords.[1], int coords.[2]))
+
+    let product3LargestCircuits =
+        getShortestCombos coords noPairs
+        |> partitionCoords
+        |> Array.map (fun p -> p.Length)
+        |> Array.sortDescending
+        |> Array.take(3)
+        |> Array.reduce (*)    
+
+    printfn $"Part 1 answer: {product3LargestCircuits}"
+    
+    let rec findClosedCircuit i =
+        let combos = getShortestCombos coords i
+        let size = getClosedCircuitSize combos
+        if size = closedCircuitSize then            
+            i-1
+        else
+            findClosedCircuit (i+1)
+
+    // Used trial and error - need to implement binary chop at some point.
+    let closedCircuitIndex = findClosedCircuit 6384 
+    let combo = (getShortestCombos coords 0).[closedCircuitIndex]
+    let extensionLength =
+        let (c1,c2) = combo
+        let (x1,_,_) = c1
+        let (x2,_,_) = c2
+        int64 x1 * int64 x2
+
+    printfn $"Part 2 answer: {extensionLength}"
+    0
---
+++ Day09/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+// Day 9: Movie Theater.
+namespace Day09
+{
+    class Program
+    {
+        static long GetArea((int,int) corner1, (int,int) corner2)
+        {
+            long length = Math.Abs(corner2.Item1 - corner1.Item1) + 1;
+            long width = Math.Abs(corner2.Item2 - corner1.Item2) + 1;
+            return length * width;
+        }
+
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("Day09.txt");
+            var corners = new List<(int, int)>();
+            foreach (var line in lines)
+            {
+                var lineSplit = line.Split(",");
+                corners.Add((int.Parse(lineSplit[0]), int.Parse(lineSplit[1])));
+            }
+            var cornerPairs = new List<(long,(int, int), (int, int))>();
+            var maxArea = 0L;
+            for (var i = 0; i < corners.Count; i++)
+            {
+                for (int j = i + 1; j < corners.Count; j++)
+                {
+                    var corner1 = corners[i];
+                    var corner2 = corners[j];
+                    var area = GetArea(corner1, corner2);
+                    cornerPairs.Add((area, corner1, corner2));
+                    if (area > maxArea) maxArea = area;
+                }
+            }
+            Console.WriteLine($"Part 1 answer: {maxArea}");
+        }
+    }
+}
